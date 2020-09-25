4c4fe3e2e8db41f196fa88107ae9522be8500be0
Gladiator07 <atharvaaingle@gmail.com>
2020-09-25 12:55:50 +0530

    voice integration

--- rasabot/actions.py
+++ rasabot/actions.py
@@ -7,21 +7,97 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List, Union
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+
+details = {
+    'ashish': '+91 8209829808',
+    'innovate': '+91 9413995563'
+}
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_your_num"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # dispatcher.utter_message(text="Hello World!")
+        print(tracker.get_slot('num'))
+        # dispatcher.utter_template('utter_your_num', tracker, number=details[str(tracker.get_slot('NAME')).lower()])
+        return []
+
+
+class ActionFormInfo(FormAction):
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+
+        return "form_info"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["NAME", "BRAND"]
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+            after all required slots are filled"""
+
+        # utter submit template
+        dispatcher.utter_message(template="utter_submit", name=tracker.get_slot('NAME'),
+                                 headset=tracker.get_slot('BRAND'))
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+
+        return {
+            "name": [self.from_entity(entity="NAME", intent='my_name_is'),
+                     self.from_text()],
+            "headset": [self.from_entity(entity="BRAND", intent="headset"),
+                        self.from_text()],
+        }
+
+    @staticmethod
+    def brand_db() -> List[Text]:
+        """Database of supported cuisines"""
+
+        return [
+            "samsung",
+            "One plus",
+            "I-phone",
+        ]
+
+    def validate_brand(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+        print(value)
+        if value.lower() in self.cuisine_db():
+            # validation succeeded, set the value of the "cuisine" slot to value
+            return {"BRAND": value}
+        else:
+            print(value)
+            dispatcher.utter_message(template="utter_wrong_value")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"BRAND": None}
---
+++ rasabot/voice_bot.py
@@ -0,0 +1,56 @@
+## Run this command in terminal  before executing this program
+## rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml
+## and also run this in seperate terminal
+## rasa run actions
+
+import requests
+import speech_recognition as sr     # import the library
+import subprocess
+from gtts import gTTS
+
+# sender = input("What is your name?\n")
+
+bot_message = ""
+message=""
+
+r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": "Hello"})
+
+print("Bot says, ",end=' ')
+for i in r.json():
+    bot_message = i['text']
+    print(f"{bot_message}")
+
+myobj = gTTS(text=bot_message)
+myobj.save("welcome.mp3")
+print('saved')
+# Playing the converted file
+subprocess.call(['mpg321', "welcome.mp3", '--play-and-exit'])
+
+while bot_message != "Bye" or bot_message!='thanks':
+
+    r = sr.Recognizer()  # initialize recognizer
+    with sr.Microphone() as source:  # mention source it will be either Microphone or audio files.
+        print("Speak Anything :")
+        audio = r.listen(source)  # listen to the source
+        try:
+            message = r.recognize_google(audio)  # use recognizer to convert our audio into text part.
+            print("You said : {}".format(message))
+
+        except:
+            print("Sorry could not recognize your voice")  # In case of voice not recognized  clearly
+    if len(message)==0:
+        continue
+    print("Sending message now...")
+
+    r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": message})
+
+    print("Bot says, ",end=' ')
+    for i in r.json():
+        bot_message = i['text']
+        print(f"{bot_message}")
+
+    myobj = gTTS(text=bot_message)
+    myobj.save("welcome.mp3")
+    print('saved')
+    # Playing the converted file
+    subprocess.call(['mpg321', "welcome.mp3", '--play-and-exit'])
