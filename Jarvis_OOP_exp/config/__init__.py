28ab6454fee72d65debe02e8dc46072e00987dde
Gladiator07 <atharvaaingle@gmail.com>
2020-11-03 20:22:46 +0530

    cleaned redundant files

---
+++ Jarvis_OOP_exp/config/__init__.py
@@ -0,0 +1,4 @@
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
--- Jarvis_OOP_exp/main.py
+++ Jarvis_OOP_exp/main.py
@@ -1,15 +1,11 @@
-import json, logging
-from assistant.jarvis import Jarvis
+import pyttsx3
+engine = pyttsx3.init()
 
-def read_config():
-    logging.info('Reading configuration data.')
-    with open('config/config.json') as file:
-        return json.load(file)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(levelname)s:%(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    jarvis = Jarvis(logger=logging, config=read_config())
-    jarvis.start()
+try:
+    engine = pyttsx3.init()
+except Exception as e:
+    print(e)
+voices = engine.getProperty('voices')
+print(voices)
+for voice in voices:
+    print(voice.id)
