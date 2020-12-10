646afa480dce54491f5f25205c971a984453f952
Gladiator07 <atharvaaingle@gmail.com>
2020-12-10 10:58:48 +0530

    added location and distance between two location feature

--- Jarvis/features/loc.py
+++ Jarvis/features/loc.py
@@ -1,4 +1,19 @@
 import webbrowser
+from geopy.geocoders import Nominatim
+from geopy.distance import great_circle
+import geocoder
 
-def loc(location):
-    webbrowser.open("http://www.google.com/maps/place/" + location + "")
+def loc(place):
+    # webbrowser.open("http://www.google.com/maps/place/" + location + "")
+    geolocator = Nominatim(user_agent="myGeocoder")
+    location = geolocator.geocode(place)
+    print(location.address)
+    target_latlng = location.latitude, location.longitude
+    print(target_latlng)
+    current_loc = geocoder.ip('me')
+    current_latlng = current_loc.latlng
+    distance = great_circle(current_latlng, target_latlng)
+    print(distance)
+    return current_loc, distance
+
+loc('Mumbai')
