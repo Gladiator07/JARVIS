f7baee49d2ad63087a1e9182d7dfc367142b732e
Gladiator07 <atharvaaingle@gmail.com>
2020-12-10 12:38:10 +0530

    remodified return data

--- Jarvis/features/loc.py
+++ Jarvis/features/loc.py
@@ -6,14 +6,14 @@ import geocoder
 def loc(place):
     # webbrowser.open("http://www.google.com/maps/place/" + location + "")
     geolocator = Nominatim(user_agent="myGeocoder")
-    location = geolocator.geocode(place)
-    print(location.address)
+    location = geolocator.geocode(place, addressdetails=True)
     target_latlng = location.latitude, location.longitude
-    print(target_latlng)
+    location = location.raw['address']
+    target_loc = {'city': location.get('city', ''),
+                   'state': location.get('state', ''),
+                   'country': location.get('country', '')}
     current_loc = geocoder.ip('me')
     current_latlng = current_loc.latlng
     distance = great_circle(current_latlng, target_latlng)
-    print(distance)
-    return current_loc, distance
+    return current_loc, target_loc, distance
 
-loc('Mumbai')
