a07b328a680e284806090dbb82013d99f5fcf3f7
Gladiator07 <atharvaaingle@gmail.com>
2020-12-14 22:05:50 +0530

    added youtube search

--- Jarvis/features/youtube_search.py
+++ Jarvis/features/youtube_search.py
@@ -1,22 +1,22 @@
-# import webbrowser, urllib, re
-# import urllib.parse
-# import urllib.request
+import webbrowser, urllib, re
+import urllib.parse
+import urllib.request
 
-# domain = input("Enter the song name: ")
-# song = urllib.parse.urlencode({"search_query" : domain})
-# print("Song" + song)
+domain = input("Enter the song name: ")
+song = urllib.parse.urlencode({"search_query" : domain})
+print("Song" + song)
 
-# # fetch the ?v=query_string
-# result = urllib.request.urlopen("http://www.youtube.com/results?" + song)
-# print(result)
+# fetch the ?v=query_string
+result = urllib.request.urlopen("http://www.youtube.com/results?" + song)
+print(result)
 
-# # make the url of the first result song
-# search_results = re.findall(r'href=\"\/watch\?v=(.{4})', result.read().decode())
-# print(search_results)
+# make the url of the first result song
+search_results = re.findall(r'href=\"\/watch\?v=(.{4})', result.read().decode())
+print(search_results)
 
-# # make the final url of song selects the very first result from youtube result
-# url = "http://www.youtube.com/watch?v="+str(search_results)
+# make the final url of song selects the very first result from youtube result
+url = "http://www.youtube.com/watch?v="+str(search_results)
 
-# # play the song using webBrowser module which opens the browser 
-# # webbrowser.open(url, new = 1)
-# webbrowser.open_new(url)
+# play the song using webBrowser module which opens the browser 
+# webbrowser.open(url, new = 1)
+webbrowser.open_new(url)
