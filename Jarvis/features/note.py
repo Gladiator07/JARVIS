7c661b342bb529b5c972101cd10359a09653a34b
Gladiator07 <atharvaaingle@gmail.com>
2021-01-12 09:52:42 +0530

    updated path

--- Jarvis/features/note.py
+++ Jarvis/features/note.py
@@ -6,6 +6,6 @@ def note(text):
     file_name = str(date).replace(":", "-") + "-note.txt"
     with open(file_name, "w") as f:
         f.write(text)
-    notepad = "C://Program Files//Notepad++//notepad++.exe"
+    notepad = "C://Program Files (x86)//Notepad++//notepad++.exe"
     subprocess.Popen([notepad, file_name])
 
